4c22b3c21d6a3678b07124152eb410a6f146d37e
micahborg <micahborghese@gmail.com>
2024-06-22 18:07:14 -0500

    create contracts

---
+++ contracts/KansasBlockchainNFT.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract KansasBlockchain is ERC721, ERC721URIStorage {
+    uint256 private _nextTokenId;
+
+    constructor()
+        ERC721("KansasBlockchain", "KBF")
+    {}
+
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
